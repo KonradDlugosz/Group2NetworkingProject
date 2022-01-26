80370ca06ac500fa627b4e8412a4baf84395ae7d
leo-hoang <leohoang92@gmail.com>
2022-01-26 16:12:32 +0000

    Added DB module to main, added personal-variables.tf

---
+++ terraform-files/personal-variables.tf
@@ -0,0 +1,11 @@
+variable "var_local_ip" {
+  default = "eu-west-1" //CHANGE IT TO YOURS
+}
+
+variable "var_zone_name" {
+  default = "group2.cyber"
+}
+
+variable "var_ami_linux_ubuntu_tf" {
+  default = "ami-08ca3fed11864d6bb"
+}
