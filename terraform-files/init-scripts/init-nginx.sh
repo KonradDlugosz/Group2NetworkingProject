d4beda236c17dfc17eb39539bb869b894ea44581
KonradDlugosz <56347935+KonradDlugosz@users.noreply.github.com>
2022-01-27 12:17:27 +0000

    Changed file ending

--- terraform-files/init-scripts/init-nginx.sh
+++ terraform-files/init-scripts/init-nginx.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 sudo apt update
 openssl req -x509 -newkey rsa:4096 -nodes -out cert.pem -keyout key.pem -days 365 -subj "/C=UK/ST=Lodon/L=London/O=SpartaGlobal/OU=Sparta/CN=Cyber/emailAddress=kdlugosz@spartaglobal.com"
 sudo systemctl reload nginx
