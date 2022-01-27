81083ddfcb3b5b64fbc46b50f7ff0d7e3727f372
KonradDlugosz <56347935+KonradDlugosz@users.noreply.github.com>
2022-01-27 12:16:22 +0000

    Added proxy config

--- terraform-files/proxy-server/variables.tf
+++ terraform-files/proxy-server/variables.tf
@@ -5,4 +5,5 @@ variable var_vpc_id_tf {}
 variable var_proxy_subnet_ip_tf {}
 variable var_public_route_table_id_tf {}
 variable var_key_file_path_tf {}
+variable var_ami_proxy_tf {}
 variable var_proxy_security_group_name_tf {default = "java10x_netproject_group2_sg_proxy"}
